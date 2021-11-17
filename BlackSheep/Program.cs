48b95259cb9f60ee7e37a51c2cef95fd743617ca
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:19:18 +0200

    more files

---
+++ BlackSheep/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace BlackSheep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Users\opilane\samples\BlackSheep.txt";
+            string[] dataFromFile = File.ReadAllLines(directoryPath);
+            for(int i = 0; i < dataFromFile.Length; i++)
+            {
+                dataFromFile[i] = dataFromFile[i].Replace('1', 'i');
+                dataFromFile[i] = dataFromFile[i].Replace('3', 'e');
+                dataFromFile[i] = dataFromFile[i].Replace('4', 'a');
+                dataFromFile[i] = dataFromFile[i].Replace('0', 'o');
+            }
+            foreach(string line in dataFromFile)
+            {
+                Console.WriteLine(line);
+            }
+            File.WriteAllLines(directoryPath, dataFromFile);
+        }
+        
+        
+            
+              
+    }
+}
---
+++ Seasons/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace Seasons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = @"C:\Users\opilane\samples\winter.txt";
+            string winterPath = @"C:\Users\opilane\samples\autumn.txt";
+            string[] dataFromFile = File.ReadAllLines(directoryPath);
+            string[] autumnData = File.ReadAllLines(winterPath);
+            File.WriteAllLines(directoryPath, autumnData);
+            File.WriteAllLines(winterPath, autumnData);
+        }
+    }
+}
